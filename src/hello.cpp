6d0350551641a3db036366a96ba276124dbd9298
chris <chris.r.jones.1983@gmail.com>
2024-11-27 11:43:23 -0600

    xerces-c: this updated example includes headers from xerces-c and uses basic functionality from xerces-c

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,9 +1,30 @@
 // a simple hello world program written in c++
 
 #include <iostream>
-
+// #include <xercesc/util/XercesDefs.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/util/XercesVersion.hpp> // For Xerces-C++ version macros
+#include <xercesc/util/XMLString.hpp> // Required for XMLString class
+                                      //
 int main() {
   std::cout << "hello world cpp" << std::endl;
 
-  return 0;
+  try {
+    // Initialize Xerces
+    xercesc::XMLPlatformUtils::Initialize();
+
+    // Example usage of XMLString for string transcoding
+    const char* sample = "Hello, Xerces!";
+    XMLCh* wideChar = xercesc::XMLString::transcode(sample);
+    std::cout << "Wide-char string: " << xercesc::XMLString::transcode(wideChar) << std::endl;
+
+    // Clean up
+    xercesc::XMLString::release(&wideChar);
+    xercesc::XMLPlatformUtils::Terminate();
+  } catch (const xercesc::XMLException& e) {
+    char* message = xercesc::XMLString::transcode(e.getMessage());
+    std::cerr << "Error during Xerces initialization: " << message << std::endl;
+    xercesc::XMLString::release(&message);
+  }
+    return 0;
 }
